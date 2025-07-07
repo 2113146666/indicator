a5540ab6e94dc69a01de0258358b96e78c22c443
2113146666 <2113146666@qq.com>
2025-07-08 00:45:43 +0800

    参数优化

--- internal/collect/getcpu.go
+++ internal/collect/getcpu.go
@@ -11,22 +11,41 @@ import (
 	"time"
 )
 
+var CPULoad = "/proc/loadavg"
+var CPUUsage = "/proc/stat"
+
 var GaugeCPUData = make(map[string]*atomic.Pointer[string])
+var loadStatSlice = []string{"cpu_load_1", "cpu_load_5", "cpu_load_15"}
 var procStatSlice = []string{"user", "nice", "system", "idle", "iowait", "irq", "softirq", "steal", "guest"}
 
 // 初始化默认值
 func init() {
-	for _, tit := range procStatSlice {
+	for _, tit := range append(procStatSlice, loadStatSlice...) {
 		GaugeCPUData[tit] = new(atomic.Pointer[string])
 		GaugeCPUData[tit].Store(common.NewStringPtr("0.00"))
 	}
+}
+
+// 获取CPU负载
+func getCPULoad() {
+	data, err := os.ReadFile(CPULoad)
+	if err != nil {
+		return
+	}
+	load_list := strings.Split(string(data), " ")
+	if len(load_list) < 3 { // 确保有3个字段
+		return
+	}
+	GaugeCPUData["cpu_load_1"].Store(common.NewStringPtr(strings.TrimSpace(load_list[0])))
+	GaugeCPUData["cpu_load_5"].Store(common.NewStringPtr(strings.TrimSpace(load_list[1])))
+	GaugeCPUData["cpu_load_15"].Store(common.NewStringPtr(strings.TrimSpace(load_list[2])))
 }
 
 // 获取数据
 func getCPUData() map[string]uint64 {
 	res := make(map[string]uint64)
 
-	data, err := os.ReadFile("/proc/stat")
+	data, err := os.ReadFile(CPUUsage)
 	if err != nil {
 		return nil
 	}
@@ -69,6 +88,9 @@ func calculateUsage(first, second map[string]uint64) {
 }
 
 func getCPUInfo() {
+	// 获取cpu负载
+	getCPULoad()
+	// 获取cpu使用率
 	firstData := getCPUData()
 	time.Sleep(1 * time.Second)
 	secondData := getCPUData()
--- internal/collect/getmeminfo.go
+++ internal/collect/getmeminfo.go
@@ -9,13 +9,23 @@ import (
 	"sync/atomic"
 )
 
+var MEMInfoCMD = "/proc/meminfo"
+
 var GaugeMEMData = map[string]*atomic.Pointer[string]{
-	"memtotal":     new(atomic.Pointer[string]),
-	"memfree":      new(atomic.Pointer[string]),
-	"buffers":      new(atomic.Pointer[string]),
-	"cached":       new(atomic.Pointer[string]),
-	"used":         new(atomic.Pointer[string]),
-	"sreclaimable": new(atomic.Pointer[string]),
+	"memtotal":         new(atomic.Pointer[string]),
+	"memfree":          new(atomic.Pointer[string]),
+	"memavailable":     new(atomic.Pointer[string]),
+	"mem_buffers":      new(atomic.Pointer[string]),
+	"mem_cached":       new(atomic.Pointer[string]),
+	"mem_active":       new(atomic.Pointer[string]),
+	"mem_inactive":     new(atomic.Pointer[string]),
+	"mem_swaptotal":    new(atomic.Pointer[string]),
+	"mem_swapcached":   new(atomic.Pointer[string]),
+	"mem_swapfree":     new(atomic.Pointer[string]),
+	"mem_shmem":        new(atomic.Pointer[string]),
+	"memused":          new(atomic.Pointer[string]),
+	"memused(%)":       new(atomic.Pointer[string]),
+	"mem_sreclaimable": new(atomic.Pointer[string]),
 }
 
 // 初始化默认值
@@ -29,7 +39,7 @@ func init() {
 // 获取数据
 func getMEMInfo() {
 
-	data, err := os.ReadFile("/proc/meminfo")
+	data, err := os.ReadFile(MEMInfoCMD)
 	if err != nil {
 		return
 	}
@@ -43,13 +53,26 @@ func getMEMInfo() {
 		if len(fields) != 2 {
 			continue
 		}
-		value, exists := GaugeMEMData[fields[0]]
-		if exists {
-			value.Store(common.NewStringPtr(fields[1]))
+		if value, exists := GaugeMEMData[fields[0]]; exists {
+			mb, err := strconv.Atoi(fields[1])
+			if err != nil {
+				continue
+			}
+			value.Store(common.NewStringPtr(strconv.Itoa(mb / 1024)))
+			continue
+		}
+		_tit := "mem_" + fields[0]
+		if value, exists := GaugeMEMData[_tit]; exists {
+			mb, err := strconv.Atoi(fields[1])
+			if err != nil {
+				continue
+			}
+			value.Store(common.NewStringPtr(strconv.Itoa(mb / 1024)))
 		}
 	}
 
 	used := common.PstringToInt(GaugeMEMData["memtotal"])
-	used -= common.PstringToInt(GaugeMEMData["memfree"]) + common.PstringToInt(GaugeMEMData["buffers"]) + common.PstringToInt(GaugeMEMData["cached"]) + common.PstringToInt(GaugeMEMData["sreclaimable"])
-	GaugeMEMData["used"].Store(common.NewStringPtr(strconv.FormatInt(used, 10)))
+	used -= common.PstringToInt(GaugeMEMData["memfree"]) + common.PstringToInt(GaugeMEMData["mem_buffers"]) + common.PstringToInt(GaugeMEMData["mem_cached"]) + common.PstringToInt(GaugeMEMData["mem_sreclaimable"])
+	GaugeMEMData["memused"].Store(common.NewStringPtr(strconv.FormatInt(used, 10)))
+	GaugeMEMData["memused(%)"].Store(common.NewStringPtr(strconv.FormatInt(used*100/common.PstringToInt(GaugeMEMData["memtotal"]), 10)))
 }
